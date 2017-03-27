db1825a928e1c92f77f08d6f44848cc3914799d0
unknown <jeferssonnunez@gmail.com>
2017-03-27 08:37:41 -0500

    Se cambia versión de hibernate a la 3, se instala y configura plugin bootstrap

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -62,9 +62,10 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.7'
         compile ":asset-pipeline:1.9.6"
+		compile ":twitter-bootstrap:3.3.4"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
+        runtime ":hibernate:3.6.10.17"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
 
