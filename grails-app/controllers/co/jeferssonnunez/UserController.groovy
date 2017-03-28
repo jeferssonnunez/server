30717017ec64925b5d29bd3dc0576a50fac5d642
unknown <jeferssonnunez@gmail.com>
2017-03-27 22:05:49 -0500

    Se modifica métodos del controlador user

--- grails-app/controllers/co/jeferssonnunez/UserController.groovy
+++ grails-app/controllers/co/jeferssonnunez/UserController.groovy
@@ -3,102 +3,129 @@ package co.jeferssonnunez
 
 
 import static org.springframework.http.HttpStatus.*
+import grails.converters.JSON
 import grails.transaction.Transactional
+import org.apache.shiro.crypto.hash.Sha256Hash
+import org.springframework.dao.DataIntegrityViolationException
 
-@Transactional(readOnly = true)
+
 class UserController {
+	def searchableService
+	
+	def permisosList = [['CODIGO':'user:*','NOMBRE':'Gestión de usuarios'],['CODIGO':'role:*','NOMBRE':'Gestión de perfiles'],['CODIGO':'sensorRegistry:*','NOMBRE':'Gestión de sensores'],
+		['CODIGO':'verticalGarden:*','NOMBRE':'Gestión de huertas'],['CODIGO':'newsItem:*','NOMBRE':'Gestión de noticias'],['CODIGO':'notification:*','NOMBRE':'Gestión de notificaciones']]
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+    static allowedMethods = [save: "POST", update: "PUT", delete: ['GET', 'POST'],create: ['GET', 'POST'], edit: ['GET', 'POST']]
 
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond User.list(params), model:[userInstanceCount: User.count()]
+    def index() {
+		params.max = Math.min(max ?: 10, 100)
+		def userInstanceList = User.list(params)
+		[userInstanceList:userInstanceList,userInstanceCount: User.count()]
     }
 
-    def show(User userInstance) {
-        respond userInstance
-    }
+    def search(){
+		params.max = Math.min(params.max ? params.int('max') : 10, 100)
+		def total = 0
+		def list = [:]
+		if (!params.query?.trim()) {
+			list = User.list(params)
+			total = User.count()
+		}else{
+			def searchResults = User.search(params.query, params)
+			total = searchResults.total
+			list = searchResults.results
+		}
+		params.remove("query")
+		render template:'listUsuarios',model:[userInstanceList:list,userInstanceCount:total]
+		return
+	}
 
     def create() {
-        respond new User(params)
+		switch (request.method) {
+			case 'GET':
+				render template:'form',model:[userInstance:new User(params)]
+			break
+			case 'POST':
+				def userInstance = new User(params)
+				userInstance.passwordHash = new Sha256Hash(params.passwordHash).toHex()
+				if (!userInstance.save(flush: true)) {
+					flash.errors = userInstance.errors
+					render 'error@'
+					render template:'errors'
+					return
+				}
+				redirect(action: "search", params: params)
+				return
+			break
+		}
     }
 
-    @Transactional
-    def save(User userInstance) {
-        if (userInstance == null) {
-            notFound()
-            return
-        }
+	def permisos(){
+		render permisosList as JSON
+	}
 
-        if (userInstance.hasErrors()) {
-            respond userInstance.errors, view:'create'
-            return
-        }
-
-        userInstance.save flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'user.label', default: 'User'), userInstance.id])
-                redirect userInstance
-            }
-            '*' { respond userInstance, [status: CREATED] }
-        }
+    def edit() {
+        switch (request.method) {
+			case 'GET':
+				def userInstance = User.get(params.id)
+				if (!userInstance) {
+					render(contentType: 'text/json'){[
+							'ErrorUpdate' : 'true',
+							'error' : 'No se encontró el usuario'
+							]}
+					return
+				}
+				render template:'form', model:[userInstance:userInstance]
+			break
+			case 'POST':
+				def userInstance = User.get(params.id)
+				if (!userInstance) {
+					render 'error@'
+					render 'No se encontró el usuario'
+					return
+				}
+				if (params.version) {
+					def version = params.version.toLong()
+					if (userInstance.version > version) {
+						render 0
+						return
+					}
+				}
+				userInstance.properties = params
+				
+				if (!userInstance.save(flush: true)) {
+					flash.errors = userInstance.errors
+					render 'error@'
+					render template:'errors'
+					return
+				}
+				redirect(action: "search", params: params)
+				return
+			break
+		}
     }
-
-    def edit(User userInstance) {
-        respond userInstance
-    }
-
-    @Transactional
-    def update(User userInstance) {
-        if (userInstance == null) {
-            notFound()
-            return
-        }
-
-        if (userInstance.hasErrors()) {
-            respond userInstance.errors, view:'edit'
-            return
-        }
-
-        userInstance.save flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'User.label', default: 'User'), userInstance.id])
-                redirect userInstance
-            }
-            '*'{ respond userInstance, [status: OK] }
-        }
-    }
-
-    @Transactional
-    def delete(User userInstance) {
-
-        if (userInstance == null) {
-            notFound()
-            return
-        }
-
-        userInstance.delete flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'User.label', default: 'User'), userInstance.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
-
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+	
+	def delete() {
+		def userInstance = User.get(params.id)
+		if (!userInstance) {
+			render(contentType: 'text/json'){[
+					'ErrorUpdate' : 'true',
+					'error' : 'No se encontró el usuario'
+					]}
+			return
+		}
+	
+		try {
+			userInstance.delete(flush: true)
+			redirect(action: "search", params: params)
+			return
+		}
+		catch (DataIntegrityViolationException e) {
+			render(contentType: 'text/json'){[
+					'ErrorUpdate' : 'true',
+					'error' : 'El usuario no pudo ser eliminado'
+					]}
+			return
+		}
+	}
 }
